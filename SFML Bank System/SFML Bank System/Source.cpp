d4f34dffb7e011d461a2ddf700a65f98fd09461f
Sian1902 <96428026+Sian1902@users.noreply.github.com>
2023-03-30 22:40:07 +0200

    second commit
    creating main entities

--- SFML Bank System/SFML Bank System/Source.cpp
+++ SFML Bank System/SFML Bank System/Source.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
 
+struct account
+{
+	string userName;
+	string email;
+	string password;
+};
+struct transaction {
+	int transactionCount;
+	string transactionType;
+	float transactionAmount;
+	string recepient;
+};
+struct user {
+	account userAccount;
+	transaction userTransaction;
+	int age;
+	float balance;
+	string phoneNumber;
+	int accountNum;
+	bool frozen=false;
 
+};
 int main() {
 	
 }
