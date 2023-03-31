45d64ef2e63c9a45325f9418b797fcc46350a556
savior131 <91454389+savior131@users.noreply.github.com>
2023-03-31 02:31:54 +0200

    savior commit

--- SFML Bank System/SFML Bank System/Source.cpp
+++ SFML Bank System/SFML Bank System/Source.cpp
@@ -27,15 +27,51 @@ struct user {
 };
 void read(vector<user> &users);
 void write(user users);
-bool find(string phoneNumber, vector<user> users);
-bool find(string email, vector<user> users);
+bool findPhone(string phoneNumber, vector<user> users);
+bool findEmail(string email, vector<user> users);
 bool find(string email, string password, vector<user> users);
 bool find(int accounNumber, vector<user> users);
+void signup(vector<user>& users);
 
 int main() {
-	
+	vector<user> users;
+	read(users);
+	signup(users);
 }
 
+void signup(vector<user>& users) {
+	user temp;
+	cout << "enter name\n";
+	cin >> temp.userAccount.userName;
+	cout << "enter email\n";
+	cin >> temp.userAccount.email;
+	while (findEmail(temp.userAccount.email, users)) {
+		cout << "email is already in use enter another one\n";
+		cin >> temp.userAccount.email;
+	}
+	cout << "enter password\n";
+	cin >> temp.userAccount.password;
+	cout << "enter phone number\n";
+	cin >> temp.phoneNumber;
+	cout << "enter age\n";
+	cin >> temp.age;
+	while (temp.age < 21) {
+		cout << "under age 7aker must be older than 21 to continue pls enter another age\n";
+		cin >> temp.age;
+	}
+	cout << "enter balance\n";
+	cin >> temp.balance;
+	while (temp.balance<300) {
+		cout << "balance can't be less than 300 EGP pls enter another amount\n";
+		cin >> temp.balance;
+	}
+
+	temp.accountNum = (rand() % 101) + 100;
+	while (find(temp.accountNum, users)) {
+		temp.accountNum = (rand() % 101) + 900;
+	}
+	write(temp);
+}
 
 void read(vector<user> &users) {
 	user temp;
@@ -62,7 +98,7 @@ void write(user users) {
 		users.balance << " " << users.transactionCount << " " << users.age << " " << users.userAccount.password << endl;
 	out.close();
 }
-bool find(string phoneNumber, vector<user> users)
+bool findPhone(string phoneNumber, vector<user> users)
 {
 	for (int i = 0; i < users.size(); i++) {
 		if (phoneNumber == users[i].phoneNumber) {
@@ -72,7 +108,7 @@ bool find(string phoneNumber, vector<user> users)
 	return false;
 }
 
-bool find(string email, vector<user> users) {
+bool findEmail(string email, vector<user> users) {
 	for (int i = 0; i < users.size(); i++) {
 		if (email == users[i].userAccount.email) {
 			return true;
